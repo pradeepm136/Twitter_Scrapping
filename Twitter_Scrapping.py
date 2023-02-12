ba00b49ec6bb5ef5a0be6e6e0f3992d43f604f77
pradeepm136 <124257061+pradeepm136@users.noreply.github.com>
2023-02-13 01:26:09 +0530

    Add files via upload

---
+++ Twitter_Scrapping.py
@@ -0,0 +1,72 @@
+# importing libraries and packages
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+import streamlit as st
+from pymongo import MongoClient
+import json
+import base64
+
+
+# Using TwitterSearchScraper to scrape data and append tweets to list
+def scraping_tweets(username, start_date, end_date, tweet_limit):
+    tweet_list = []
+    for i, tweet in enumerate(
+        sntwitter.TwitterSearchScraper('{}'.format(username)).get_items()):  # declare a username or search keyword
+        data = (
+        [tweet.date, tweet.id, tweet.content, tweet.user.username, tweet.likeCount, tweet.retweetCount, tweet.url,
+         tweet.replyCount, tweet.lang, tweet.source])
+        tweet_list.append(data)
+        if i == tweet_limit - 1:
+            break
+
+def create_df(tweets_list): #Creating dataframe
+    df1 = pd.DataFrame(tweets_list,
+        columns=['Date', 'Tweet ID', 'Tweet Content', 'Tweet Username', 'Like Count', 'Retweet Count', 'URL',
+                            'Reply Count', 'Language', 'Source'])
+    return df1
+
+
+# Using streamlit for GUI
+st.title("Scrape the Tweets")
+
+# Get input from user for username/hashtag or search keywords
+hashtag = st.text_input("Enter the hashtag:")
+start_date = st.date_input("Select start date:", key="start_date")
+end_date = st.date_input("Select end date:", key="end_date")
+tweet_limit = st.number_input("Enter the number of tweet you need:", key="limit")
+
+
+# Scrape tweets
+
+if st.button("Scrape Tweets"):
+    tweet = scraping_tweets(hashtag, start_date, end_date, tweet_limit)
+    tweet_data = create_df(tweet)
+    st.dataframe(tweet_data)
+
+    # Download as csv
+
+    st.write("Saving dataframe as csv")
+    csv = tweet_data.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="tweet_data.csv">Download CSV File</a>'
+    st.markdown(href, unsafe_allow_html=True)
+
+    # Download as JSON
+
+    st.write("Saving dataframe as json")
+    json_string = tweet_data.to_json(indent=2)
+    b64 = base64.b64encode(json_string.encode()).decode()
+    href = f'<a href="data:file/json;base64,{b64}" download="tweet_data.json">Download json File</a>'
+    st.markdown(href, unsafe_allow_html=True)
+
+    # Upload to mongoDB
+if st.button("upload to MongoDB"):
+    tweet = scraping_tweets(hashtag, start_date, end_date, tweet_limit)
+    tweet_data = create_df(tweet)
+
+    client = MongoClient('mongodb://localhost:27017')
+    db = client["twitter_db_streamlit"]
+    collection = db['tweet']
+    tweet_data_json = json.loads(tweet_data.to_json(orient='records'))
+    collection.insert_many(tweet_data_json)
+    st.success('uploaded to MongoDB')
