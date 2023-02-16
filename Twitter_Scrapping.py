723e51e643e3b3e8b66313f363c0bfe2dc6295f6
pradeepm136 <124257061+pradeepm136@users.noreply.github.com>
2023-02-16 06:36:00 +0530

    Update Twitter_Scrapping.py

--- Twitter_Scrapping.py
+++ Twitter_Scrapping.py
@@ -72,5 +72,3 @@ if st.button("upload to MongoDB"):
     tweet_data_json = json.loads(tweet_data.to_json(orient='records'))
     collection.insert_many(tweet_data_json)
     st.success('uploaded to MongoDB')
-else:
-    null
