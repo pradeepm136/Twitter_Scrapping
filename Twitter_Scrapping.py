521cbca98336fb0ca0282bb048ac9b05aad92d9d
pradeepm136 <124257061+pradeepm136@users.noreply.github.com>
2023-02-18 09:26:12 +0530

    Update Twitter_Scrapping.py

--- Twitter_Scrapping.py
+++ Twitter_Scrapping.py
@@ -67,9 +67,9 @@ if st.button("Upload to MongoDB"):
     tweet = scraping_tweets(hashtag, start_date, end_date, tweet_limit)
     tweet_data = create_df(tweet)
 
-    client = MongoClient('mongodb+srv://username:password@cluster0.hwjaa45.mongodb.net/test')
+    client = MongoClient('mongodb+srv://pradeep:password@cluster0.hwjaa45.mongodb.net/test')
     db = client["Twitter_db_streamlit"]
-    collection = db['Tweet']
+    collection = db[hashtag]
     tweet_data_json = json.loads(tweet_data.to_json(orient='records'))
     collection.insert_many(tweet_data_json)
     st.success('Uploaded to MongoDB')
